7ac0fe6cbccba1d331e8c322f9a679b941753a72
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-23 18:04:01 -0400

    built header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <body className="App-container">
+        <Header />
+      </body>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <header>
+            <h1>
+                Welcome to AInstant Help!
+            </h1>
+        </header>
+    )
+} 
