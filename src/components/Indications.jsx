d34b78f33af29c37e0e8a14c74365a1ccc89ee14
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-23 18:06:47 -0400

    build the indications

--- src/components/Indications.jsx
+++ src/components/Indications.jsx
@@ -1,7 +1,11 @@
 export default function Indications(){
     return(
         <>
-            
+            <p>Welcome to our AI-based therapy app! Our platform offers a safe and confidential space for you to discuss your mental health concerns with an AI-based therapist that uses GPT-3 technology. Our app aims to provide personalized and supportive guidance to help you manage and overcome your mental health issues.</p>
+
+            <p>
+            To use our app, simply type in your concern or issue in the prompt box and hit send. Our AI-based therapist will generate a response in real-time, simulating a real human therapist. You can continue the conversation by responding to the generated message. The more information you provide, the more personalized and effective the response will be. Please keep in mind that while our app offers guidance and support, it is not a replacement for professional therapy. If you are in crisis or need urgent help, please contact your local emergency services or seek help from a licensed mental health professional.
+            </p>
         </>
     )
 }
