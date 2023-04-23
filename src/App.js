a616c945bfedb36ebe616618938377c3c1373fc9
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-23 18:13:08 -0400

    fixed indications

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Indications from './components/Indications';
 
 function App() {
   return (
     <div className="App">
       <body className="App-container">
         <Header />
+        <Indications />
       </body>
     </div>
   );
