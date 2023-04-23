c6728be8c4f3ac594b5027c10b7ba0ace8f9225f
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-23 19:34:51 -0400

    added input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Header from './components/Header';
 import Indications from './components/Indications';
+import Result from './components/Result';
+import textField from './components/textField';
 
 function App() {
   return (
@@ -8,6 +10,9 @@ function App() {
       <body className="App-container">
         <Header />
         <Indications />
+        <textField />
+        <Result />
+        
       </body>
     </div>
   );
---
+++ src/components/textField.jsx
@@ -0,0 +1,37 @@
+import { Input } from "antd"
+
+export default function textField() {
+
+    const handleAdd = async (value) => {
+        if (value.length <4) return 
+       
+        const newRequest = {
+            prompt: value
+        }
+        const response = await fetch('http://localhost:3000/request', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newRequest),
+        })
+        const data = await response.json()
+
+    }
+    return (
+        <section>
+        
+            <Input.Search
+            allowClear
+            enterButton= "Add"
+            size= "large"
+            onSearch={handleAdd}
+            placeholder="Write your condern here"
+            />
+    
+        </section>
+    
+        
+    
+      )
+}
